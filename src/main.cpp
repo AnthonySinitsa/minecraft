f2a7513bca2c5c943ee0de57f583433fe9939c2d
AnthonySinitsa <itonyemail@gmail.com>
2023-10-06 01:31:01 -0700

    adding window and opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,70 @@
+#include <SDL.h>
+#include <GL/glew.h>
 #include <iostream>
+#include <glm/glm.hpp>  // Include GLM after GLEW
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Set OpenGL version to 3.3 core profile
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    // Create SDL window
+    SDL_Window *window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
+    if (window == nullptr) {
+        std::cerr << "Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Create OpenGL context
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (context == nullptr) {
+        std::cerr << "Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // Initialize GLEW
+    glewExperimental = GL_TRUE;
+    GLenum glewError = glewInit();
+    if (glewError != GLEW_OK) {
+        std::cerr << "Error: " << glewGetErrorString(glewError) << std::endl;
+        return -1;
+    }
+
+    // Set up OpenGL, load shaders, set up buffers, etc.
+    // ...
+
+    // Main loop
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+            // Additional event handling
+        }
+
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw the cube
+        // ...
+
+        // Swap the window buffers
+        SDL_GL_SwapWindow(window);
+    }
+
+    // Clean up
+    SDL_GL_DeleteContext(context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
